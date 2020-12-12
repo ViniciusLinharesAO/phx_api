199940852f2e44d7f3d75a7fbf1d2b8ee8bc3f3d
Vinícius Linhares <vinicius@ar.quei.ro>
2020-12-12 18:50:04 -0300

    adding users model

---
+++ lib/my_app/account/user.ex
@@ -0,0 +1,25 @@
+defmodule MyApp.Account.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "users" do
+    field :email, :string
+    field :is_active, :boolean, default: false
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  @fields [:email, :is_active, :password]
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, @fields)
+    |> validate_required(@fields)
+    |> unique_constraint(:email)
+  end
+end
